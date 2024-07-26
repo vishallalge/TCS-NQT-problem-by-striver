84f07379637b46023add02a75e4e8caa10e5c3e8
vishallalge <vishallalgecollege7249@gmail.com>
2024-07-27 00:15:06 +0530

    next questions solved

--- Striver coding questions/16_Maximum_Product_Subarray_in_an_Array.cpp
+++ Striver coding questions/16_Maximum_Product_Subarray_in_an_Array.cpp
@@ -1,33 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int max1(int a, int b){
-    int ans;
-    if(a>=b){
-        ans = a;
-    }
-    else{
-        ans = b;
-    }
-    return ans;
-}
-
+// int max1(int a, int b){
+//     int ans;
+//     if(a>=b){
+//         ans = a;
+//     }
+//     else{
+//         ans = b;
+//     }
+//     return ans;
+// }
 
 //brute force approach
-int maxSubarrayProduct(int nums[], int n){
-    int result = nums[0];
-    for(int i=0;i<n-1;i++) {
-        int p = nums[i];
-        for(int j=i+1;j<n;j++) {
-           result = max1(result,p);
-           p *= nums[j];
+int maxSubarrayProduct1(int arr[], int n){
+    int result = INT_MIN;
+    for(int i = 0; i<n; i++){
+        int p = arr[i];
+        for(int j = i+1; j<n; j++){
+            result = max(result, p);
+            p *= arr[j];
         }
-        result = max1(result,p);  //manages (n-1)th term
+        result = max(result, p);
     }
     return result;
 }
 
+//optimal approach 
+int maxSubarrayProduct2(int arr[], int n){
+    int ans = INT_MIN;
+    int pre = 1, suff = 1;
+    for(int i = 0; i<n; i++){
+        if(pre == 0) pre = 1;
+        if(suff == 0) suff = 1;
+        pre *= arr[i];
+        suff *= arr[n-i-1];
+        ans = max(ans, max(pre, suff));
+    }
+    return ans;
+}
+
 int main(){
     int arr[] = {1,2,3,4,5,0};
+    int n = size(arr);
+    cout<<"1. Product of subarray is: "<<maxSubarrayProduct1(arr, n)<<endl;
+    cout<<"2. Product of subarray is: "<<maxSubarrayProduct2(arr, n)<<endl;
     return 0;
 }
